f27fddf0a9b2173ab75c285aa6c2a5ade32a1402
Muneeb Ahmad <muneeb.ahmad@thoughtworks.com>
2024-07-17 17:49:49 +0200

    refactor(rust): Update Stack pop

--- rust/ds-course/src/structs/stacks.rs
+++ rust/ds-course/src/structs/stacks.rs
@@ -12,7 +12,7 @@ impl<T> Stack<T> {
     pub fn new(size: usize) -> Self {
         let mut arr = HeapArray::with_capacity(size);
         Self {
-            top: NonNull::new(arr.get_ptr() as *mut T).unwrap(),
+            top: NonNull::new(arr.as_ptr_mut()).unwrap(),
             size,
             length: 0,
             data: arr
@@ -24,24 +24,22 @@ impl<T> Stack<T> {
         self.length += 1;
         let ptr: *mut T;
         if self.length > 1 {
-            ptr = unsafe { self.data.get_ptr().add(self.data.get_len() - 1) as *mut T };
+            ptr = unsafe { self.data.as_ptr_mut().add(self.data.get_len() - 1) };
         } else {
-            ptr = self.data.get_ptr() as *mut T;
+            ptr = self.data.as_ptr_mut();
         }
         self.top = NonNull::new(ptr).unwrap();
     }
 
-    pub fn pop(&mut self) -> T where T: Default {
-        let data = self.data.pop();
-        self.length -= 1;
-        let ptr: *mut T;
-        if self.length > 1 {
-            ptr = unsafe { self.data.get_ptr().add(self.data.get_len() - 1) as *mut T };
-        } else {
-            ptr = self.data.get_ptr() as *mut T;
+    pub fn pop(&mut self) -> Option<T> where T: Default {
+        if self.length == 0 {
+            return None
         }
+        let data = self.data.pop().unwrap();
+        self.length -= 1;
+        let ptr = unsafe { self.data.as_ptr_mut().add(self.data.get_len() - 1) };
         self.top = NonNull::new(ptr).unwrap();
-        data
+        Some(data)
     }
 
     pub fn peek(&self, index: usize) -> &T {
@@ -76,7 +74,7 @@ mod stack {
     #[test]
     fn test_new() {
         let stack: Stack<u8> = Stack::new(5);
-        assert_eq!(stack.top, NonNull::new(stack.data.get_ptr() as *mut u8).unwrap(), "Stack top pointer is invalid!");
+        assert_eq!(stack.top, NonNull::new(stack.data.as_ptr_mut()).unwrap(), "Stack top pointer is invalid!");
         assert_eq!(stack.size, 5, "Stack size is invalid!");
         assert_eq!(stack.length, 0, "Stack length is invalid!");
         assert_eq!(stack.data, HeapArray::new(), "Stack array is invalid!");
@@ -86,7 +84,7 @@ mod stack {
     fn test_push() {
         let mut stack: Stack<u8> = Stack::new(5);
         stack.push(1);
-        assert_eq!(stack.top, NonNull::new(stack.data.get_ptr() as *mut u8).unwrap(), "Stack top pointer is invalid!");
+        assert_eq!(stack.top, NonNull::new(stack.data.as_ptr_mut()).unwrap(), "Stack top pointer is invalid!");
         assert_eq!(stack.size, 5, "Stack size is invalid!");
         assert_eq!(stack.length, 1, "Stack length is invalid!");
         assert_eq!(format!("{}", stack.data), "[1, 0, 0, 0, 0]".to_string(), "Stack array is invalid!");
@@ -95,10 +93,11 @@ mod stack {
     #[test]
     fn test_pop() {
         let mut stack: Stack<u8> = Stack::new(5);
+        assert_eq!(stack.pop(), None, "Empty stack pop value is invalid!");
         stack.push(1);
         stack.push(2);
-        assert_eq!(stack.pop(), 2, "Stack pop value is invalid!");
-        assert_eq!(stack.top, NonNull::new(stack.data.get_ptr() as *mut u8).unwrap(), "Stack top pointer is invalid!");
+        assert_eq!(stack.pop(), Some(2), "Stack pop value is invalid!");
+        assert_eq!(stack.top, NonNull::new(stack.data.as_ptr_mut()).unwrap(), "Stack top pointer is invalid!");
         assert_eq!(stack.length, 1, "Stack length is invalid!");
         assert_eq!(format!("{}", stack.data), "[1, 0, 0, 0, 0]".to_string(), "Stack array is invalid!");
     }
