04d3c7db486fdf84e4e11bb6069c5cef8d5df110
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2024-08-05 13:58:31 +0200

    refactor(rust): Fix formatting for Polynomials

--- rust/ds-course/src/structs/polynomials.rs
+++ rust/ds-course/src/structs/polynomials.rs
@@ -1,11 +1,11 @@
+use crate::structs::arrays::HeapArray;
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
-use crate::structs::arrays::HeapArray;
 
 #[derive(Debug)]
 struct Term {
     coefficient: i32,
-    exponent: u32
+    exponent: u32,
 }
 
 impl Display for Term {
@@ -18,7 +18,7 @@ impl Display for Term {
 #[derive(Debug)]
 pub struct Polynomial {
     count: usize,
-    terms: HeapArray<Term>
+    terms: HeapArray<Term>,
 }
 
 impl Display for Polynomial {
@@ -36,14 +36,16 @@ impl Display for Polynomial {
 impl PartialEq for Polynomial {
     fn eq(&self, other: &Self) -> bool {
         if self.count != other.count {
-            return false
+            return false;
         }
         for i in 0..self.count {
-            if self.terms[i].coefficient != other.terms[i].coefficient || self.terms[i].exponent != other.terms[i].exponent {
-                return false
+            if self.terms[i].coefficient != other.terms[i].coefficient
+                || self.terms[i].exponent != other.terms[i].exponent
+            {
+                return false;
             }
         }
-        return true
+        return true;
     }
 
     fn ne(&self, other: &Self) -> bool {
@@ -54,23 +56,20 @@ impl PartialEq for Polynomial {
 impl Polynomial {
     pub fn new(count: usize) -> Self {
         let terms = HeapArray::with_capacity(count);
-        Polynomial {
-            count,
-            terms
-        }
+        Polynomial { count, terms }
     }
 
     pub fn set_term(&mut self, coefficient: i32, exponent: u32) {
         if self.terms.get_len() == self.terms.get_size() {
-            self.terms.resize(self.terms.get_len() + 1).expect("Failed to increase the Polynomial terms count!");
+            self.terms
+                .resize(self.terms.get_len() + 1)
+                .expect("Failed to increase the Polynomial terms count!");
             self.count += 1
         }
-        self.terms.push(
-            Term {
-                coefficient,
-                exponent
-            }
-        );
+        self.terms.push(Term {
+            coefficient,
+            exponent,
+        });
     }
 
     pub fn evaluate(&self, x: i32) -> i32 {
@@ -78,7 +77,7 @@ impl Polynomial {
         for i in 0..self.count {
             res += self.terms[i].coefficient * x.pow(self.terms[i].exponent)
         }
-        return res
+        return res;
     }
 
     pub fn add(&mut self, other: Self) {
@@ -92,7 +91,10 @@ impl Polynomial {
 
         while i < self.count && j < other.count {
             if self.terms[i].exponent == other.terms[j].exponent {
-                res.set_term(self.terms[i].coefficient + other.terms[j].coefficient, self.terms[i].exponent);
+                res.set_term(
+                    self.terms[i].coefficient + other.terms[j].coefficient,
+                    self.terms[i].exponent,
+                );
                 i += 1;
                 j += 1;
             } else if self.terms[i].exponent > other.terms[i].exponent {
@@ -106,7 +108,9 @@ impl Polynomial {
 
         self.count = res.terms.get_len();
         self.terms = res.terms;
-        self.terms.resize(self.count).expect("Failed to resize the Polynomial terms array!");
+        self.terms
+            .resize(self.count)
+            .expect("Failed to resize the Polynomial terms array!");
     }
 }
 
@@ -118,8 +122,16 @@ mod polynomial {
         let count: usize = 5;
         let poly: Polynomial = Polynomial::new(count);
         assert_eq!(poly.count, 5, "Invalid polynomial terms count!");
-        assert_eq!(poly.terms.get_len(), 0, "Invalid Polynomial terms array length. It should be 0!");
-        assert_eq!(poly.terms.get_size(), count, "Invalid Polynomial terms array size. It should be set to the terms count!");
+        assert_eq!(
+            poly.terms.get_len(),
+            0,
+            "Invalid Polynomial terms array length. It should be 0!"
+        );
+        assert_eq!(
+            poly.terms.get_size(),
+            count,
+            "Invalid Polynomial terms array size. It should be set to the terms count!"
+        );
     }
 
     #[test]
@@ -127,9 +139,17 @@ mod polynomial {
         let mut poly: Polynomial = Polynomial::new(2);
         poly.set_term(4, 2);
         poly.set_term(1, 1);
-        assert_eq!(poly.terms.get_len(), 2, "Invalid Polynomial terms array length. It should be 2!");
+        assert_eq!(
+            poly.terms.get_len(),
+            2,
+            "Invalid Polynomial terms array length. It should be 2!"
+        );
         poly.set_term(2, 0);
-        assert_eq!(poly.terms.get_len(), 3, "Failed to extend the Polynomial length!");
+        assert_eq!(
+            poly.terms.get_len(),
+            3,
+            "Failed to extend the Polynomial length!"
+        );
         assert_eq!(poly.count, 3, "Failed to update the Polynomial count!");
     }
 
@@ -141,7 +161,11 @@ mod polynomial {
         poly.set_term(3, 2);
         poly.set_term(4, 1);
         poly.set_term(3, 0);
-        assert_eq!(poly.evaluate(x), 23, "Invalid Polynomial evaluation. Outcome should be 2!");
+        assert_eq!(
+            poly.evaluate(x),
+            23,
+            "Invalid Polynomial evaluation. Outcome should be 2!"
+        );
     }
 
     #[test]
@@ -169,9 +193,20 @@ mod polynomial {
 
         p1.add(p2);
 
-        assert_eq!(p1.count, 4, "Invalid Polynomial count after addition. Outcome should be 4!");
-        assert_eq!(p1.terms.get_len(), 4, "Invalid Polynomial terms array length after addition. Outcome should be 4!");
-        assert_eq!(p1.terms.get_size(), 4, "Invalid Polynomial terms array size after addition. Outcome should be 4!");
+        assert_eq!(
+            p1.count, 4,
+            "Invalid Polynomial count after addition. Outcome should be 4!"
+        );
+        assert_eq!(
+            p1.terms.get_len(),
+            4,
+            "Invalid Polynomial terms array length after addition. Outcome should be 4!"
+        );
+        assert_eq!(
+            p1.terms.get_size(),
+            4,
+            "Invalid Polynomial terms array size after addition. Outcome should be 4!"
+        );
         assert_eq!(p1, res, "Invalid Polynomial addition!");
     }
 }
